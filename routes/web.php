e2bc6631acf01f9bfe7516bacfa2249078f63fff
esteban bernal <cristober24@gmail.com>
2024-01-06 15:14:00 -0300

    agregue route

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,28 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+ Route::get('/texto',function(){
+    return "<h1>App Laravel 1</h1>";
+ });
+ Route::get('/arreglo',function(){
+
+    $arreglo=['id'=>'1','descripcion'=>'producto 1'];
+    return $arreglo;
+ });
+
+ Route::get('/cliente/{cliente?}',function($cliente='cliente NN'){
+  
+    return'<h1>El Cliente es: '.$cliente.'</h1>';
+ });
+
+ Route::get('/ruta1',function(){
+   return '<h1>Route 1</h1>';
+ })->name('rutaNro1');
+
+ Route::get('/ruta2',function(){
+   return redirect()->route('rutaNro1');
+ });
+
+ Route::get('/usuario/{usuario}',function($usuario){
+   return '<h1>User: '.$usuario.'</h1>';
+ })->where('usuario','[0-9]+');
